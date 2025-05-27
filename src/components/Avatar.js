cf959d956b3ec28cd5ff50cf74d877f01496c515
Simone <simonehuening09@gmail.com>
2025-05-27 10:20:12 +0200

    changing shirt

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,13 +1,33 @@
 import "../styles/Avatar.css";
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Avatar (){
+    const[avatarState, setAvatarState]= useState({
+        shirts: {current: 0, total: 3}
+    })
+
+    function next(item){
+        let nextNum = avatarState[item].current + 1;
+        let newCurrent = nextNum < avatarState[item].total ? nextNum: 0;
+        setAvatarState({
+            ...avatarState,
+            [item]:{
+                current: avatarState[item].current=newCurrent,
+                total: avatarState[item].total
+            }
+        })
+    }
+
+
     return (
         <div id="avatarContainer">
             <div id="background">
                 <div id="base"></div>
-                <div id="shirt" className="shirt1"></div>
+                <div id="shirt" className={"shirt"+(avatarState["shirts"].current+1)}></div>
             </div>
+            <input type="button" value="next shirt" id="nextshirt" onClick={() => next("shirts")}/>
         </div>
     );
 }
