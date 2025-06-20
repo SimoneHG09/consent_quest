9ac5b5be24aa759b3ee7222aea933688a6f8cb56
Simone <simonehuening09@gmail.com>
2025-06-20 22:40:33 +0200

    Avatar almost done

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 
 function Avatar({onContinue}) {
     const [avatarState, setAvatarState] = useState({
-        shirts: { current: 0, total: 3 }
+        shirts: { current: 0, total: 10 },
+        faces: {current: 0, total: 7},
+        hair: {curren:0, total:19}
     });
 
     function next(item) {
@@ -20,24 +22,60 @@ function Avatar({onContinue}) {
         });
     }
 
+    function last(item) {
+        setAvatarState(prevState => {
+            const lastNum = prevState[item].current - 1;
+            const newCurrent = lastNum >= 0 ? lastNum : prevState[item].total - 1;
+            return {
+                ...prevState,
+                [item]: {
+                    current: newCurrent,
+                    total: prevState[item].total
+                }
+            };
+        });
+    }
+
     return (
         <div className="avatarContainer">
-            <div className="phone-container">
+            <div className="arrowContainer">
+                <div className="phoneContainer">
                 <img src="/images/cards/phone.png" alt="Phone frame" className="phone-frame" />
                 <div className="screen">
-                    <div className="face-container">
-                        <div id="base"></div>
-                        <div id="shirt" className={`shirt${avatarState.shirts.current + 1}`}></div>
-                    </div>
-                    
+                     <div className="screenTitle">Choose your Character</div>
+                    <div className="faceContainer">
+                    <div id="base" className={`base${avatarState.faces.current + 1}`}></div>
+                    <div id="shirt" className={`shirt${avatarState.shirts.current + 1}`}></div>
+                    <div id="hair" className={`hair${avatarState.hair.current + 1}`}></div>
+                    </div>   
                 </div>
             </div>
-            <button id="nextScreen" onClick={onContinue}>Continue</button>
-            <button 
-                className="arrow-button" 
-                onClick={() => next("shirts")}
-                aria-label="Next shirt"
-            ></button>
+                <button className="nextScreen" onClick={onContinue}>Continue</button>
+                <button 
+                    className="arrowButton hairLeft" 
+                    onClick={() => last("hair")}
+                ></button>
+                <button 
+                    className="arrowButton hairRight" 
+                    onClick={() => next("hair")}
+                ></button>
+                <button 
+                    className="arrowButton facesLeft" 
+                    onClick={() => last("faces")}
+                ></button>
+                <button 
+                    className="arrowButton facesRight" 
+                    onClick={() => next("faces")}
+                ></button>
+                <button 
+                    className="arrowButton shirtsLeft" 
+                    onClick={() => last("shirts")}
+                ></button>
+                <button 
+                    className="arrowButton shirtsRight" 
+                    onClick={() => next("shirts")}
+                ></button>
+            </div>
         </div>
     );
 }
